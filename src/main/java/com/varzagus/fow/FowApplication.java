af6f37fc4c9ad6464df4495c78e7a7e0f5a5b3b2
VarZagus <69350747+VarZagus@users.noreply.github.com>
2021-11-09 01:21:29 +0300

    Перенос проекта на Spring

---
+++ src/main/java/com/varzagus/fow/FowApplication.java
@@ -0,0 +1,13 @@
+package com.varzagus.fow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varzagus/fow/FowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varzagus.fow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
